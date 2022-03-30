64383cb08caa917a2ad6c9457c459ec810dad877
Bence Puschmann <bence.puschmann@lombiq.com>
2022-03-30 23:12:20 +0200

    Update Treasury.sol
    Adding modifier to contract to check if the current user is the owner

--- contracts/Treasury.sol
+++ contracts/Treasury.sol
@@ -4,14 +4,19 @@ pragma solidity ^0.8.13;
 
 import "hardhat/console.sol";
 
-contract Treasury {    
-    address treasuryOwner;
-    uint256 treasuryBalance;
+contract Treasury { 
+    address public treasuryOwner;
+    uint256 public goalToReach;
     mapping(address => uint256) depositedMoneyByAccount;
-    mapping(address => bool) public isConnectedToTreasury;
+
 
     constructor(){
         treasuryOwner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == treasuryOwner, "Only the owner of the contract is allowed to execute this");
+        _;
     }
 
     event NewTransaction(address indexed from, uint timestamp, uint256 amount, string transactionType);
@@ -23,40 +28,40 @@ contract Treasury {
         string transactionType;
     }
 
-    function joinToTreasury() external {
-        isConnectedToTreasury[msg.sender] = true;
+    Deposition[] depositions;
+
+    function setGoalToReach(uint256 _goal) external onlyOwner{
+        goalToReach = _goal;
     }
 
     function depositMoney() public payable {
         require(msg.value != 0, "You need to deposit some amount of money!");
-        treasuryBalance += msg.value;
         depositedMoneyByAccount[msg.sender] += msg.value;
+
+        depositions.push(Deposition(msg.sender, msg.value, block.timestamp, "Deposition"));
 
         emit NewTransaction(msg.sender, block.timestamp, msg.value, "Deposition");
     }
 
     function getTreasuryAccountBalance() external view returns(uint256){
-        return treasuryBalance;
+        return address(this).balance;
     }
 
-    function withdrawMoney(address payable _to, uint _amount) public {
+    function sendToOwner() public payable onlyOwner{
         require(
-            _amount <= treasuryBalance,
-            "You have insuffient funds to withdraw"
-        );
-        (bool sent, ) = _to.call {value: _amount}("");
+             address(this).balance < goalToReach,
+            "Goal not reached yet");
+
+        (bool sent, ) = treasuryOwner.call {value: address(this).balance}("");
 
         require(sent, "Transaction failed");
-        treasuryBalance -= _amount;
 
-        emit NewTransaction(msg.sender, block.timestamp, _amount, "Withdrawal");
+         depositions.push(Deposition(treasuryOwner, address(this).balance, block.timestamp, "Withdrawal"));
+
+        emit NewTransaction(treasuryOwner, block.timestamp, address(this).balance, "Withdrawal");
     }
 
     function userContribution() external view returns(uint256){
         return depositedMoneyByAccount[msg.sender];
-    }
-
-    function IsConnected() external view returns(bool){
-        return isConnectedToTreasury[msg.sender];
     }
 }
